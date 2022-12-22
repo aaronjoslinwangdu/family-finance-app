552879367f088930248e3c0569d0c874ddd4c71d
Aaron Joslin-Wangdu <aaronjoslinwangdu@gmail.com>
2022-12-21 20:43:33 -0500

    Add error and async handlers

--- server/controllers/ExpenseController.js
+++ server/controllers/ExpenseController.js
@@ -1,30 +1,39 @@
+const asyncHandler = require('express-async-handler');
+
+
 // @desc    Get an expense
 // @route   GET /api/expenses
 // @access  Private
-const getExpenses = (req, res) => {
+const getExpenses = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Get Expense' });
-}
+});
 
 // @desc    Set an expense
 // @route   POST /api/expenses
 // @access  Private
-const setExpense = (req, res) => {
+const setExpense = asyncHandler(async (req, res) => {
+
+  if (!req.body.text) {
+    res.status(400)
+    throw new Error('Please add a valid Expense');
+  }
+
   res.status(200).json({ message: 'Set Expense' });
-}
+});
 
 // @desc    Update an expense
 // @route   PUT /api/expenses/:id
 // @access  Private
-const updateExpense = (req, res) => {
+const updateExpense = asyncHandler( async (req, res) => {
   res.status(200).json({ message: `Update Expense ${req.params.id}` });
-}
+});
 
 // @desc    Delete an expense
 // @route   GET /api/expenses/:id
 // @access  Private
-const deleteExpense = (req, res) => {
+const deleteExpense = asyncHandler( async (req, res) => {
   res.status(200).json({ message: `Delete Expense ${req.params.id}` });
-}
+});
 
 module.exports = {
   getExpenses,
--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
-const app = express();
+const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
+const { errorHandler } = require('./middleware/ErrorMiddleware');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/expenses', require('./routes/ExpenseRoutes'));
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
