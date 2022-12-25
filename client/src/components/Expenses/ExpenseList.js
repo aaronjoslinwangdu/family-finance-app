cd8d2044e5f7b09226b0d51c5f44d6af395a96ff
Aaron Joslin-Wangdu <aaronjoslinwangdu@gmail.com>
2022-12-24 20:44:18 -0500

    Fetch expenses

--- client/src/components/Expenses/ExpenseList.js
+++ client/src/components/Expenses/ExpenseList.js
@@ -4,6 +4,25 @@ import ExpenseSublist from './ExpenseSublist';
 
 const ExpenseList = (props) => {
 
+  fetch('http://localhost:5000/api/expenses')
+  .then((response) => {
+    if (!response.ok) {
+      return response.json()
+        .then((data) => {
+          let errorMessage = 'Authentication Error';
+          throw new Error(errorMessage);
+        });
+    } else {
+      return response.json();
+    }
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    alert(error.message);
+  });
+
   return (
     <div>
       <ExpenseSublist />
