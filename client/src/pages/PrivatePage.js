1b5af02c5da4abd51e4e29a1d79e5931c18b26ff
Aaron Joslin-Wangdu <aaronjoslinwangdu@gmail.com>
2023-02-02 20:41:04 -0500

    Add private page

---
+++ client/src/pages/PrivatePage.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+const PrivatePage = (props) => {
+  const history = useHistory();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // check if user is authenticated
+  useEffect(() => {
+
+    fetch('/private', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: {
+        accessToken: localStorage.getItem('accessToken')
+      }
+    })
+    .then((res) => {
+      if (res.status === 200) {
+        setIsAuthenticated(true);
+        console.log('auth success')
+      } else {
+        setIsAuthenticated(false);
+        console.log('auth fail');
+      }
+    });
+  }, []);
+
+  return (
+    <React.Fragment>
+      {isAuthenticated && <div>{props.children}</div>}
+    </React.Fragment>
+  );
+};
+
+export default PrivatePage;
